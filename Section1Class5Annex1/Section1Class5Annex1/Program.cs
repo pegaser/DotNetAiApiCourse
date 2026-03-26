b8620c5890ee5ed3cade4cb43800321ce6655d19
pegaser <sperezgabriel@gmail.com>
2026-03-25 20:19:48 -0600

    Section 1 Class 5 Annex 1

---
+++ Section1Class5Annex1/Section1Class5Annex1/Program.cs
@@ -0,0 +1,34 @@
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+string? apiKey = builder.Configuration["AISettings:ApiKey"];
+if (string.IsNullOrEmpty(apiKey))
+{
+    Console.WriteLine("No se encontró la clave de API. Verifica la configuración.");
+}
+else
+{
+    Console.WriteLine("Clave de API cargada correctamente.");
+}
+
+var app = builder.Build();
+
+app.UseStaticFiles();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+
+app.MapGet("/api/support/status", () =>
+{
+    return Results.Ok(new
+    {
+        message = "Servicio de Soporte IA funcionando correctamente."
+    });
+});
+
+app.Run();
